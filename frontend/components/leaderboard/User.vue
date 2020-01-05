fee6fc54322752aacba45fcfdd5046a2e3bb4ca2
Thomas Castleman <thomas7rex@gmail.com>
2020-01-05 01:09:08 -0500

    Add an okay looking leaderboard if I don't say so myself

---
+++ frontend/components/leaderboard/User.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <span class="title text-truncate mr-10">
+        <nuxt-link
+          :to="`/user/${user.uid}`"
+          class="name teal--text text--accent-2"
+        >
+          {{ user.name }}
+        </nuxt-link>
+      </span>
+      <v-spacer></v-spacer>
+      <div>
+        <span class="subtitle-2 mr-3">
+          Rank
+        </span>
+        <span class="headline font-weight-bold teal--text text--accent-2">
+          {{ user.userRank }}
+        </span>
+        <span class="grey--text text--lighten-1 mx-2">|</span>
+        <span class="headline font-weight-bold teal--text text--accent-2">
+          {{ user.score }}
+        </span>
+        <span class="subtitle-2 ml-3">Score</span>
+      </div>
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'User',
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.name {
+  text-decoration: none !important;
+}
+.name:hover {
+  color: #a7ffeb !important;
+}
+</style>
---
+++ frontend/pages/leaderboard/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container fluid>
+    <v-row v-for="item in leaderboard" :key="key(item)" justify="center">
+      <v-col cols="12" md="7">
+        <User :user="item" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import User from '@/components/leaderboard/User.vue'
+
+export default {
+  components: {
+    User
+  },
+  async asyncData({ app, error }) {
+    try {
+      const leaderboard = await app.$axios.$get('/users/leaderboard')
+      return { leaderboard }
+    } catch (err) {
+      error(err)
+    }
+  },
+  methods: {
+    key(item, index) {
+      return `user-${item.uid}`
+    }
+  }
+}
+</script>
