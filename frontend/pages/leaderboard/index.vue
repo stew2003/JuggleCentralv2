81995d4d71006633390751ef681c93a340ec699e
Thomas Castleman <thomas7rex@gmail.com>
2020-01-12 16:48:13 -0500

    Start patterrn page

--- frontend/pages/leaderboard/index.vue
+++ frontend/pages/leaderboard/index.vue
@@ -1,8 +1,22 @@
 <template>
   <v-container fluid>
-    <v-row v-for="item in leaderboard" :key="key(item)" justify="center">
-      <v-col cols="12" md="7" class="py-1">
-        <User :user="item" />
+    <v-row justify="center">
+      <v-col cols="12" md="7">
+        <v-card>
+          <v-card-title class="display-2">Leaderboard</v-card-title>
+          <v-divider></v-divider>
+          <v-card-text>
+            <v-row
+              v-for="item in leaderboard"
+              :key="key(item)"
+              justify="center"
+            >
+              <v-col class="py-1">
+                <User :user="item" />
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
---
+++ frontend/pages/pattern/_id/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container fluid>
+    <v-row justify="center">
+      <v-col cols="12" md="7">
+        <v-card>
+          <v-card-title class="display-2">{{ pattern.name }}</v-card-title>
+          <v-divider></v-divider>
+          <span>{{ pattern.description }}</span>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  async asyncData({ params, app }) {
+    const pattern = await app.$axios.$get(`/pattern?uid=${params.id}`)
+    return { pattern }
+  }
+}
+</script>
