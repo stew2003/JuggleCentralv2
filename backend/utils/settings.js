ec41a5376d5952964724dc5c779d83110d4a1279
Thomas Castleman <thomas7rex@gmail.com>
2020-04-30 17:58:31 -0400

    changed port so it doesn't fight with my other mysql

--- backend/utils/settings.js
+++ backend/utils/settings.js
@@ -1,7 +1,7 @@
 module.exports = {
   // amount by which to scale up user scores (higher scale, greater precision, also more excitement)
-  USER_SCORE_SCALING_FACTOR: 100,
+  USER_SCORE_SCALING_FACTOR: 100
   /*	Required domain for new user account emails.
 		Use a regular expression that will match any email from your domain. */
-  EMAIL_DOMAIN_RESTRICTION: /.+?@(students\.)?stab\.org/
+  // EMAIL_DOMAIN_RESTRICTION: /.+?@(students\.)?stab\.org/
 }
